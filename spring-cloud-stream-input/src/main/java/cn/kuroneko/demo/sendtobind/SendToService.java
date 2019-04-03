b985c230ddef5ce3a7916487adfacc4398e18c3f
Kuronekoo <kuroneko.su@outlook.com>
2019-04-03 17:14:15 +0800

    增加注释

--- spring-cloud-stream-input/src/main/java/cn/kuroneko/demo/sendtobind/SendToService.java
+++ spring-cloud-stream-input/src/main/java/cn/kuroneko/demo/sendtobind/SendToService.java
@@ -10,7 +10,7 @@ public class SendToService {
     @StreamListener(SendToBinder.SENDBINDER_INPUT)
     @SendTo(SendToBinder.SENDBINDER_OUTPUT)
     public Object receiveFromInput(Object payload){
-        System.out.println("中转消息。。"+payload);
+        System.out.println("中转消息.."+payload);
         return "xxxxx";
     }
 }
